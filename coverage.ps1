43dccf7bc7f0faf0e62d1d33a6aaf472f5272315
Petr Švihlík <svihlik.petr@gmail.com>
2019-10-31 21:56:23 +0100

    removed unnecessary files

--- coverage.ps1
+++ coverage.ps1
@@ -1,17 +1,15 @@
-$ENV:CONFIGURATION = if ($ENV:CONFIGURATION -eq $null) { "Release" } else { $ENV:CONFIGURATION }
+$buildConfig = if ($ENV:CONFIGURATION -eq $null) { "Release" } else { $ENV:CONFIGURATION }
 
-$ENV:APPVEYOR_BUILD_FOLDER = if ($ENV:APPVEYOR_BUILD_FOLDER -eq $null) { $PSScriptRoot } else { $ENV:APPVEYOR_BUILD_FOLDER }
+$buildFolder = if ($ENV:APPVEYOR_BUILD_FOLDER -eq $null) { $PSScriptRoot } else { $ENV:APPVEYOR_BUILD_FOLDER }
 
 $openCover = 'C:\ProgramData\chocolatey\lib\opencover.portable\tools\OpenCover.Console.exe'
 
-ForEach ($folder in (Get-ChildItem -Path .\ -Directory -Filter *.Tests)) { 
-    $project = Get-ChildItem -Path ($folder.FullName+"\*.csproj") | Select-Object -First 1
-    
-    $logger = ("--logger:trx;LogFileName=" + $folder.FullName + "\results.trx")
-    dotnet test $project.FullName
-    
-    $targetArgs = '-targetargs: test ' + $project.FullName + ' ' +  ' -c ' + $ENV:CONFIGURATION + ' ' + $logger + ' /p:DebugType=full' 
-    $filter = '-filter:+[Wopi*]*-[*Tests]*'
-    & $openCover '-target:C:\Program Files\dotnet\dotnet.exe' $targetArgs '-register:user' $filter '-oldStyle' '-mergeoutput' ('-output:' + $ENV:APPVEYOR_BUILD_FOLDER + '\coverage.xml')
-	
-    }
+dotnet test
+
+$target = '-target:C:\Program Files\dotnet\dotnet.exe'
+$targetArgs = '-targetargs:"test -c:' + $buildConfig + ' --logger:trx;LogFileName=results.trx /p:DebugType=full"' 
+$filter = '-filter:+[Wopi*]*-[*Tests]*'
+$output = '-output:' + $buildFolder + '\coverage.xml'
+$register = if ($ENV:APPVEYOR -eq $true ) { '-register' } else { '-register:user' } # Magical parameter that fucks up things
+
+& $openCover $target $targetArgs $filter $register '-oldStyle' '-mergeoutput' $output
