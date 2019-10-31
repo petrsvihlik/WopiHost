13faad7928848d126c08d8be3391c4a055282207
Petr Švihlík <svihlik.petr@gmail.com>
2019-10-31 20:32:43 +0100

    better params handling

--- coverage2.ps1
+++ coverage2.ps1
@@ -1,14 +1,18 @@
-$ENV:CONFIGURATION = if ($ENV:CONFIGURATION -eq $null) { "Release" } else { $ENV:CONFIGURATION }
+$buildConfig = if ($ENV:CONFIGURATION -eq $null) { "Release" } else { $ENV:CONFIGURATION }
 
-$ENV:APPVEYOR_BUILD_FOLDER = if ($ENV:APPVEYOR_BUILD_FOLDER -eq $null) { $PSScriptRoot } else { $ENV:APPVEYOR_BUILD_FOLDER }
+$buildFolder = if ($ENV:APPVEYOR_BUILD_FOLDER -eq $null) { $PSScriptRoot } else { $ENV:APPVEYOR_BUILD_FOLDER }
 
 $openCover = 'C:\ProgramData\chocolatey\lib\opencover.portable\tools\OpenCover.Console.exe'
-dotnet --version
+
 dotnet test
 
-$logger = ("--logger:trx;LogFileName=results.trx")    
-$targetArgs = '-targetargs:" test -c ' + $ENV:CONFIGURATION + ' ' + $logger + ' /p:DebugType=full"' 
+$target = '-target:C:\Program Files\dotnet\dotnet.exe'
+$targetArgs = '-targetargs:"test -c:' + $buildConfig + ' --logger:trx;LogFileName=results.trx /p:DebugType=full"' 
 $filter = '-filter:+[Wopi*]*-[*Tests]*'
-& $openCover '-target:C:\Program Files\dotnet\dotnet.exe' $targetArgs '-register:user' $filter '-oldStyle' '-mergeoutput' ('-output:' + $ENV:APPVEYOR_BUILD_FOLDER + '\coverage.xml')
+$output = '-output:' + $buildFolder + '\coverage.xml'
+
+
+
+& $openCover $target $targetArgs $filter '-register:user' '-oldStyle' '-mergeoutput' $output
 	
   
