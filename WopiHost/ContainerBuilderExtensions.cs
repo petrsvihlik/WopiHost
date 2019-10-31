f20d3289bd2bffc463c870b623c7523a710762a1
Petr Švihlík <svihlik.petr@gmail.com>
2019-10-31 22:53:44 +0100

    adjusted project files

--- WopiHost/ContainerBuilderExtensions.cs
+++ WopiHost/ContainerBuilderExtensions.cs
@@ -9,6 +9,8 @@ namespace WopiHost
         public static void AddFileProvider(this ContainerBuilder builder, string storageProviderAssemblyName)
         {
             // Load file provider
+            //TODO: load by name? AssemblyLoadContext.Default.LoadFromAssemblyName
+            //TODO: Unloadability https://docs.microsoft.com/en-us/dotnet/standard/assembly/unloadability
             var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath($"{AppContext.BaseDirectory}\\{storageProviderAssemblyName}.dll");
             builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces();
         }
