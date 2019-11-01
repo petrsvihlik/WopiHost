aeee6322e1cb621336474a48ce5eac45dd8debe3
Petr Švihlík <svihlik.petr@gmail.com>
2019-11-01 01:55:41 +0100

    new tests

---
+++ WopiHost.Url.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace WopiHost.Url.Tests
+{
+    public class CollectionExtensionsTests
+    {
+        [Fact]
+        public void MergeWorksWithNulls()
+        {
+            Dictionary<string, string> full = new Dictionary<string, string>();
+            Dictionary<string, string> empty = null;
+
+            var one = full.Merge(empty);
+            var two = empty.Merge(full);
+
+            Assert.Equal(full, one);
+            Assert.Equal(full, two);
+        }
+    }
+}
--- WopiHost.Url/CollectionExtensions.cs
+++ WopiHost.Url/CollectionExtensions.cs
@@ -3,25 +3,23 @@ using System.Linq;
 
 namespace WopiHost.Url
 {
-	public static class CollectionExtensions
-	{
-		/// <summary>
-		/// Merges two dictionaries. If duplicate occurs, <param name="dictA"></param> wins.
-		/// </summary>
-		/// <param name="dictA">Primary dictionary</param>
-		/// <param name="dictB">Secondary dictionary</param>
-		/// <returns></returns>
-		public static IDictionary<TKey, TValue> Merge<TKey, TValue>(this IDictionary<TKey, TValue> dictA, IDictionary<TKey, TValue> dictB) where TValue : class
-		{
-			if (dictA == null)
-			{
-				return dictB;
-			}
-			if (dictB == null)
-			{
-				return dictA;
-			}
-			return dictA.Keys.Union(dictB.Keys).ToDictionary(k => k, k => dictA.ContainsKey(k) ? dictA[k] : dictB[k]);
-		}
-	}
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Merges two dictionaries. If duplicate occurs, <param name="dictA"></param> wins over <param name="dictB" />.
+        /// </summary>
+        /// <returns></returns>
+        public static IDictionary<TKey, TValue> Merge<TKey, TValue>(this IDictionary<TKey, TValue> dictA, IDictionary<TKey, TValue> dictB) where TValue : class
+        {
+            if (dictA == null)
+            {
+                return dictB;
+            }
+            if (dictB == null)
+            {
+                return dictA;
+            }
+            return dictA.Keys.Union(dictB.Keys).ToDictionary(k => k, k => dictA.ContainsKey(k) ? dictA[k] : dictB[k]);
+        }
+    }
 }
