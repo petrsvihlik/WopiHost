c1b9de1b0fa190984de7dc434f5cc13e30e3e8a2
Petr Švihlík <svihlik.petr@gmail.com>
2019-11-01 02:05:50 +0100

    test non-null dictionaries

--- WopiHost.Url.Tests/CollectionExtensionsTests.cs
+++ WopiHost.Url.Tests/CollectionExtensionsTests.cs
@@ -8,14 +8,32 @@ namespace WopiHost.Url.Tests
         [Fact]
         public void MergeWorksWithNulls()
         {
+            // Arrange
             Dictionary<string, string> full = new Dictionary<string, string>();
             Dictionary<string, string> empty = null;
 
+            // Act
             var one = full.Merge(empty);
             var two = empty.Merge(full);
 
+            // Assert
             Assert.Equal(full, one);
             Assert.Equal(full, two);
+        }
+
+        [Fact]
+        public void MergeTwoDictionaries()
+        {
+            // Arrange
+            Dictionary<string, string> a = new Dictionary<string, string> { { "A", "B"}, { "C", "D" } };
+            Dictionary<string, string> b = new Dictionary<string, string> { { "G", "H" }, { "I", "J" } };
+
+            // Act
+            var result = a.Merge(b);
+
+            // Assert
+            Assert.Contains("A", result);
+            Assert.Contains("G", result);
         }
     }
 }
