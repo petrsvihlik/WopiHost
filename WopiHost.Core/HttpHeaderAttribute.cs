62f9d2a6f45027c18b6505c1f908a458663c8725
Petr Švihlík <svihlik.petr@gmail.com>
2020-10-25 15:33:46 +0100

    updated nugets + fixed the header constraint + moved to net5

--- WopiHost.Core/HttpHeaderAttribute.cs
+++ WopiHost.Core/HttpHeaderAttribute.cs
@@ -4,22 +4,34 @@ using System.Linq;
 
 namespace WopiHost.Core
 {
+    /// <summary>
+    /// A header-based constraint for HTTP actions.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
     public class HttpHeaderAttribute : Attribute, IActionConstraint
     {
-        public string Header { get; set; }
-        public string[] Values { get; set; }
+        private string Header { get; set; }
 
+        private string[] Values { get; set; }
+
+        /// <summary>
+        /// Creates an instance of a constraint based on a header name and allowed values.
+        /// </summary>
+        /// <param name="header">Header name to check.</param>
+        /// <param name="values">Accepted header values.</param>
         public HttpHeaderAttribute(string header, params string[] values)
         {
             Header = header;
             Values = values;
         }
 
+        /// <inheritdoc />
         public bool Accept(ActionConstraintContext context)
         {
-            return (context is null) && (context.RouteContext.HttpContext.Request.Headers.TryGetValue(Header, out var value) && Values.Contains(value[0]));
+            return (context is not null) && context.RouteContext.HttpContext.Request.Headers.TryGetValue(Header, out var value) && Values.Contains(value[0]);
         }
 
+        /// <inheritdoc />
         public int Order => 0;
     }
 }
