405aa9557c76691cc87f0c9e373f0a283e9431cd
dvojitywendy <36680598+dvojitywendy@users.noreply.github.com>
2020-10-28 23:26:12 +0100

    Added comments for required WOPI properties (#107)

--- WopiHost.Core/Models/CheckFileInfo.cs
+++ WopiHost.Core/Models/CheckFileInfo.cs
@@ -8,14 +8,30 @@
         //TODO: and https://wopi.readthedocs.io/en/latest/scenarios/customization.html?highlight=checkfileinfo
         #region "Required properties"
 
+        /// <summary>
+        /// The string name of the file, including extension, without a path. Used for display in user interface (UI), and determining the extension of the file.
+        /// </summary>
         public string BaseFileName { get; set; }
 
+        /// <summary>
+        /// A string that uniquely identifies the owner of the file. In most cases, the user who uploaded or created the file should be considered the owner. This ID is subject to uniqueness and consistency requirements. See <see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/CheckFileInfo.html?highlight=checkfileinfo#user-identity-requirements">Requirements for user identity properties</see> for more information.
+        /// </summary>
         public string OwnerId { get; set; }
 
+        /// <summary>
+        /// The size of the file in bytes, expressed as a long, a 64-bit signed integer.
+        /// </summary>
         public long Size { get; set; }
 
+        /// <summary>
+        /// A string value uniquely identifying the user currently accessing the file. This ID is subject to uniqueness and consistency requirements. See <see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/CheckFileInfo.html?highlight=checkfileinfo#user-identity-requirements">Requirements for user identity properties</see> for more information.
+        /// </summary>
         public string UserId { get; set; }
 
+        /// <summary>
+        /// The current version of the file based on the server’s file version schema, as a string. This value must change when the file changes, and version values must never repeat for a given file.
+        /// This value must be a string, even if numbers are used to represent versions.
+        /// </summary>
         public string Version { get; set; }
 
         #endregion
@@ -104,26 +120,145 @@
 
         public bool SupportsCoauth { get; set; }
 
+        /// <summary>
+        /// A Boolean value that indicates that the host supports the following WOPI operations:
+        /// <list type="bullet">
+        /// <item>
+        /// <description>ExecuteCellStorageRequest</description>
+        /// </item>
+        /// <item>
+        /// <description>ExecuteCellStorageRelativeRequest</description>
+        /// </item>
+        /// </list>
+        /// These operations are only used by OneNote for the web and are thus not needed to integrate with Office for the web or Office for iOS. These are included for completeness but do not need to be implemented.
+        /// </summary>
         public bool SupportsCobalt { get; set; }
 
+        /// <summary>
+        /// A Boolean value that indicates that the host supports the following WOPI operations:
+        /// <list type="bullet">
+        /// <item>
+        /// <description>CheckFolderInfo - This operation is only used by OneNote for the web and is thus not needed to integrate with Office for the web or Office for iOS. It is included for completeness but does not need to be implemented.</description>
+        /// </item>
+        /// <item>
+        /// <description>EnumerateChildren (folders) - This operation is only used by OneNote for the web and is thus not needed to integrate with Office for the web or Office for iOS. It is included for completeness but does not need to be implemented.</description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/DeleteFile.html#deletefile">DeleteFile</see></description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        /// <value></value>
         public bool SupportsFolders
         {
             get => SupportsContainers;
             set => SupportsContainers = value;
         }
 
+        /// <summary>
+        /// A Boolean value that indicates that the host supports the following WOPI operations:
+        /// <list type="bullet">
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/containers/CheckContainerInfo.html#checkcontainerinfo">CheckContainerInfo</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/containers/CreateChildContainer.html#createchildcontainer">CreateChildContainer</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/containers/CreateChildFile.html#createchildfile">CreateChildFile</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/containers/DeleteContainer.html#deletecontainer">DeleteContainer</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/DeleteFile.html#deletefile">DeleteFile</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/containers/EnumerateAncestors.html#enumerateancestors-containers">EnumerateAncestors (containers)</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/EnumerateAncestors.html#enumerateancestors-files">EnumerateAncestors (files)</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/containers/EnumerateChildren.html#enumeratechildren">EnumerateChildren (containers)</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/containers/GetEcosystem.html#getecosystem-containers">GetEcosystem (containers)</see></description>
+        /// </item>
+        ///  <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/containers/RenameContainer.html#renamecontainer">RenameContainer</see></description>
+        /// </item>
+        /// </list>
+        /// </summary>
         public bool SupportsContainers { get; set; }
 
+        /// <summary>
+        /// A Boolean value that indicates that the host supports the following WOPI operations:
+        /// <list type="bullet">
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/Lock.html#lock">Lock</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/Unlock.html#unlock">Unlock</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/RefreshLock.html#refreshlock">RefreshLock</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/UnlockAndRelock.html#unlockandrelock">UnlockAndRelock</see> operations for this file.</description>
+        /// </item>
+        /// </list>
+        /// </summary>
         public bool SupportsLocks { get; set; }
 
+        /// <summary>
+        /// A Boolean value that indicates that the host supports the <see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/GetLock.html#getlock">GetLock</see> operation.
+        /// </summary>
         public bool SupportsGetLock { get; set; }
 
+        /// <summary>
+        /// A Boolean value that indicates that the host supports lock IDs up to 1024 ASCII characters long. If not provided, WOPI clients will assume that lock IDs are limited to 256 ASCII characters.
+        /// </summary>
         public bool SupportsExtendedLockLength { get; set; }
 
+        /// <summary>
+        /// A Boolean value that indicates that the host supports the following WOPI operations:
+        /// <list type="bullet">
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/ecosystem/CheckEcosystem.html#checkecosystem">CheckEcosystem</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/containers/GetEcosystem.html#getecosystem-containers">GetEcosystem (containers)</see></description>
+        /// </item>
+        ///  <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/GetEcosystem.html#getecosystem-files">GetEcosystem (files)</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/ecosystem/GetRootContainer.html#getrootcontainer">GetRootContainer (ecosystem)</see></description>
+        /// </item>
+        /// </list>
+        /// </summary>
         public bool SupportsEcosystem { get; set; }
 
+        /// <summary>
+        /// A Boolean value that indicates that the host supports the <see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/ecosystem/GetFileWopiSrc.html#getfilewopisrc">GetFileWopiSrc (ecosystem)</see> operation.
+        /// </summary>
         public bool SupportsGetFileWopiSrc { get; set; }
 
+        /// <summary>
+        /// An array of strings containing the <see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/concepts.html#term-share-url">Share URL</see> types supported by the host.
+        /// These types can be passed in the X-WOPI-UrlType request header to signify which Share URL type to return for the <see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/GetShareUrl.html#getshareurl-files">GetShareUrl (files)</see> operation.
+        /// <para> Possible Values:
+        /// <list type="bullet">
+        /// <item>
+        /// <description>ReadOnly - This type of Share URL allows users to view the file using the URL, but does not give them permission to edit the file.</description>
+        /// </item>
+        /// <item>
+        /// <description>ReadWrite - This type of Share URL allows users to both view and edit the file using the URL.</description>
+        /// </item>
+        /// </list>
+        /// </para>
+        /// </summary>
         public string[] SupportedShareUrlTypes { get; set; }
 
         public bool SupportsScenarioLinks { get; set; }
@@ -132,12 +267,32 @@
 
         public bool SupportsFileCreation { get; set; }
 
+        /// <summary>
+        /// A Boolean value that indicates that the host supports the following WOPI operations:
+        /// <list type="bullet">
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/PutFile.html#putfile">PutFile</see></description>
+        /// </item>
+        /// <item>
+        /// <description><see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/PutRelativeFile.html#putrelativefile">PutRelativeFile</see></description>
+        /// </item>
+        /// </list>
+        /// </summary>
         public bool SupportsUpdate { get; set; }
 
+        /// <summary>
+        /// A Boolean value that indicates that the host supports the <see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/RenameFile.html#renamefile">RenameFile</see> operation.
+        /// </summary>
         public bool SupportsRename { get; set; }
 
+        /// <summary>
+        /// A Boolean value that indicates that the host supports the <see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/DeleteFile.html#deletefile">DeleteFile</see> operation.
+        /// </summary>
         public bool SupportsDeleteFile { get; set; }
 
+        /// <summary>
+        /// A Boolean value that indicates that the host supports the <see href="https://wopi.readthedocs.io/projects/wopirest/en/latest/files/PutUserInfo.html#putuserinfo">PutUserInfo</see> operation.
+        /// </summary>
         public bool SupportsUserInfo { get; set; }
 
         public string UserInfo { get; set; }
