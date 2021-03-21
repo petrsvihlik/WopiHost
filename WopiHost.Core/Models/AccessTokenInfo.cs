10f5d5aaed1287ba7b4c28ba2726bc9c6a54c073
Petr Švihlík <rocky.intel@gmail.com>
2021-03-21 01:58:10 +0100

    added comments

--- WopiHost.Core/Models/AccessTokenInfo.cs
+++ WopiHost.Core/Models/AccessTokenInfo.cs
@@ -1,9 +1,19 @@
 ﻿namespace WopiHost.Core.Models
 {
+	/// <summary>
+	/// Represents a WOPI access token.
+	/// </summary>
 	public class AccessTokenInfo
 	{
+		/// <summary>
+		/// A string access token for the file specified in the X-WOPI-WopiSrc request header.
+		/// </summary>
 		public string AccessToken { get; set; }
 
+		/// <summary>
+		/// A long value representing the time that the access token provided in the response will expire. 
+		/// See access_token_ttl for more information on how this value is defined.
+		/// </summary>
 		public long AccessTokenExpiry { get; set; }
 	}
 }
