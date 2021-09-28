0b91a3ce9f8b820ffcdaddcbb75b7284e6bcf3d1
Petr Švihlík <rocky.intel@gmail.com>
2021-09-28 21:18:18 +0200

    linux file owner identification

--- WopiHost.FileSystemProvider/WopiFile.cs
+++ WopiHost.FileSystemProvider/WopiFile.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Runtime.Versioning;
 using System.Security.Principal;
 using WopiHost.Abstractions;
 
@@ -79,12 +80,29 @@ namespace WopiHost.FileSystemProvider
         }
 
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// A string that uniquely identifies the owner of the file.
+        /// Supported only on Windows and Linux.
+        /// https://docs.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca1416
+        /// </summary>
+        [SupportedOSPlatform("linux")]
+        [SupportedOSPlatform("windows")]
         public string Owner
         {
             get
             {
-                return FileInfo.GetAccessControl().GetOwner(typeof(NTAccount)).ToString();
+                if (OperatingSystem.IsWindows())
+                {
+                    return FileInfo.GetAccessControl().GetOwner(typeof(NTAccount)).ToString();
+                }
+                else if (OperatingSystem.IsLinux())
+                {
+                    return Mono.Unix.UnixFileSystemInfo.GetFileSystemEntry(FilePath).OwnerUser.UserName; //TODO: test
+                }
+                else
+                {
+                    return "UNSUPPORTED_PLATFORM";
+                }
             }
         }
     }
