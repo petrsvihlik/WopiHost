a149e696b92343588c51576b3c6388a018c428cb
codefactor-io <support@codefactor.io>
2025-03-06 16:49:24 +0000

    [CodeFactor] Apply fixes

--- sample/WopiHost.Validator/Pages/Index.cshtml.cs
+++ sample/WopiHost.Validator/Pages/Index.cshtml.cs
@@ -49,7 +49,6 @@ public class IndexModel(
             var files = storageProvider.GetWopiFiles(ContainerId);
             foreach (var file in files)
             {
-                
                 Files.Add(new FileViewModel
                 {
                     FileId = file.Identifier,
