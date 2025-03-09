ea0c1a3444cc632a4387f9abb0995ba711ffaa10
Petr Švihlík <rocky.intel@gmail.com>
2025-03-09 16:23:28 +0100

    Update FilesController.cs

--- src/WopiHost.Core/Controllers/FilesController.cs
+++ src/WopiHost.Core/Controllers/FilesController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Mime;
 using System.Security.Claims;
 using System.Text.Json;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
